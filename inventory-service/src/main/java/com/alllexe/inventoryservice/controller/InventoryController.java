60c042bfc608c4b4aaa676f419df5eda6793ab09
Абрамов Александр <a.abramov@voskhod.ru>
2022-08-03 10:30:57 +0300

    add order via rest request to inventory service

--- inventory-service/src/main/java/com/alllexe/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/alllexe/inventoryservice/controller/InventoryController.java
@@ -1,13 +1,16 @@
 package com.alllexe.inventoryservice.controller;
 
+import com.alllexe.inventoryservice.dto.InventoryResponse;
 import com.alllexe.inventoryservice.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/inventory")
@@ -16,10 +19,10 @@ public class InventoryController {
 
     private final InventoryService inventoryService;
 
-    @GetMapping("/{sku-code}")
+    @GetMapping()
     @ResponseStatus(HttpStatus.OK)
-    public boolean isInStock(@PathVariable("sku-code") String skuCode) {
-        return inventoryService.isInStock(skuCode);
+    public List<InventoryResponse> isInStock(@RequestParam List<String> skuCodes) {
+        return inventoryService.isInStock(skuCodes);
     }
 
 }
--- order-service/src/main/java/com/alllexe/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/alllexe/orderservice/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.alllexe.orderservice.service;
 
+import com.alllexe.orderservice.dto.InventoryResponse;
 import com.alllexe.orderservice.dto.OrderLineItemsDto;
 import com.alllexe.orderservice.dto.OrderRequest;
 import com.alllexe.orderservice.model.Order;
@@ -8,7 +9,9 @@ import com.alllexe.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -17,6 +20,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final WebClient webClient;
 
     @Transactional
     public void placeOrder(OrderRequest orderRequest) {
@@ -28,7 +32,27 @@ public class OrderService {
                 .toList();
 
         order.setOrderLineItems(orderLineItems);
-        orderRepository.save(order);
+
+        List<String> skuCodes = order.getOrderLineItems().stream()
+                .map(OrderLineItems::getSkuCode)
+                .toList();
+
+        // Call Inventory Service, and place order if product is in stock
+        InventoryResponse[] inventoryResponseArray = webClient.get()
+                .uri("http://localhost:8082/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCodes", skuCodes).build())
+                .retrieve()
+                .bodyToMono(InventoryResponse[].class)
+                .block();
+
+        boolean allProductsInStock = Arrays.stream(inventoryResponseArray)
+                .allMatch(InventoryResponse::isInStock);
+
+        if (allProductsInStock) {
+            orderRepository.save(order);
+        } else {
+            throw new IllegalArgumentException("Product is not in stock");
+        }
     }
 
     private OrderLineItems mapToEntity(OrderLineItemsDto orderLineItemsDto) {
